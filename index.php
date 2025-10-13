cc9177a6071075286d0ba2e9b34531551e2915ee
David Perera <davidperera2006@gmail.com>
2025-10-13 18:04:52 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,30 @@
-<body>
-        <h1>Projecte Welcome 1</h1>
-        <ul>
-        <?php
-        $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-        foreach( $imgs as $img ) {
-            if( $img=="." || $img==".." )
-                continue;
-            if( substr($img,-3)=="jpg" or substr($img,-3)=="png"){
-                $name = substr($img,0,-4);
-            }else if (substr($img,-4)=="jpeg") {
-                $name = substr($img,0,-5);
-            }
-            echo "<a href='profile/$name.html'>";
-            echo "<img src='img/$img' width='130'>";
-            echo $name."</a>";
-            echo "<div></div>";
 
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project Welcome</title>
+</head>
+<body>
+    <h1>Project Welcome</h1>
+    <ul>
+        <?php
+        $profiles = scandir("./profile",SCANDIR_SORT_ASCENDING);
+        foreach($profiles as $profile) {
+            if ($profile === "." || $profile === ".."){
+                continue;
+            }
+            echo "<div>";
+            $name = substr($profile,0,-5);
+            echo "<a href='profile/$profile'>";
+            $img = glob("img/$name.*");
+            $img = $img ? $img[0] : "";
+            echo "<img src='$img' width='130' alt='$name'>";
+            echo $name."</a>";
+            echo "</div>";
         }
-    ?>
-    </il>
+        ?>
+    </ul>
 </body>
+</html>
